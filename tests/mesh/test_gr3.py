99086e70bf0f13ce9d52b2293eab81a95f00b9a5
J.R. Calzada <jreniel@gmail.com>
2020-01-14 10:50:04 -0500

    improvements to test file

--- tests/mesh/test_gr3.py
+++ tests/mesh/test_gr3.py
@@ -108,54 +108,12 @@ class Gr3TestCase(unittest.TestCase):
         reader(pathlib.Path(tmpfile.name))
 
     def test_gr3_overwrite(self):
-        nodes = {
-            0: ((0., 0.), -99999.),
-            1: ((.5, 0.), -99999.),
-            2: ((0., 1.), -99999.),
-            3: ((1., 1.), -99999.),
-            4: ((0., 1.), -99999.),
-            5: ((.5, 1.5), -99999.),
-            6: ((.33, .33), -99999.),
-            7: ((.66, .33), -99999.),
-            8: ((.5, .66), -99999.),
-            9: ((-1., 1), -99999.),
-            10: ((-1, 0.), -99999.),
-        }
-        elements = {
-            0: [4, 6, 8],
-            1: [0, 1, 6],
-            2: [1, 2, 7],
-            3: [7, 6, 1],
-            4: [2, 3, 7],
-            5: [3, 8, 7],
-            6: [3, 5, 4],
-            7: [4, 9, 10, 0],
-            8: [8, 3, 4],
-            9: [4, 0, 6]
-        }
-
-        boundaries = dict()
-        boundaries[None] = {  # "open" boundaries
-            0: [9, 10, 0, 1],
-            1: [1, 2, 3]
-        }
-        boundaries[0] = {  # "land" boundaries
-            0: [3, 5],
-            1: [5,  4, 9]
-        }
-        boundaries[1] = {0: [6, 7, 8]}  # "interior" boundary
-        grd = {
-            'nodes': nodes,
-            'elements': elements,
-            'boundaries': boundaries,
-            'description': 'gr3.py unittest'
-        }
         tmpdir = tempfile.TemporaryDirectory()
-        writer(grd, pathlib.Path(tmpdir.name) / 'hgrid.gr3')
+        writer(self.grd, pathlib.Path(tmpdir.name) / 'hgrid.gr3')
         self.assertRaises(
             Exception,
             writer,
-            grd,
+            self.grd,
             pathlib.Path(tmpdir.name) / 'hgrid.gr3'
             )
 
