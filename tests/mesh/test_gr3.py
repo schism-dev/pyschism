140d8bac245bc48415c58c4105d5af9f459cd6d6
J.R. Calzada <jreniel@gmail.com>
2020-01-15 09:09:48 -0500

    Corrected test to account for file format inconsistencies

--- tests/mesh/test_gr3.py
+++ tests/mesh/test_gr3.py
@@ -9,44 +9,44 @@ class Gr3TestCase(unittest.TestCase):
 
     def setUp(self):
         self.nodes = {
-            1: ((0., 0.), -99999.),
-            2: ((.5, 0.), -99999.),
-            3: ((0., 1.), -99999.),
-            4: ((1., 1.), -99999.),
-            5: ((0., 1.), -99999.),
-            6: ((.5, 1.5), -99999.),
-            7: ((.33, .33), -99999.),
-            8: ((.66, .33), -99999.),
-            9: ((.5, .66), -99999.),
-            10: ((-1., 1), -99999.),
-            11: ((-1, 0.), -99999.),
+            '1': ((0., 0.), -99999.),
+            '2': ((.5, 0.), -99999.),
+            '3': ((0., 1.), -99999.),
+            '4': ((1., 1.), -99999.),
+            '5': ((0., 1.), -99999.),
+            '6': ((.5, 1.5), -99999.),
+            '7': ((.33, .33), -99999.),
+            '8': ((.66, .33), -99999.),
+            '9': ((.5, .66), -99999.),
+            '10': ((-1., 1.), -99999.),
+            '11': ((-1., 0.), -99999.),
             }
         self.elements = {
-            1: [5, 7, 9],
-            2: [1, 2, 7],
-            3: [2, 3, 8],
-            4: [8, 7, 2],
-            5: [3, 4, 8],
-            6: [4, 9, 8],
-            7: [4, 6, 5],
-            8: [5, 10, 11, 1],
-            9: [9, 4, 5],
-            10: [5, 1, 7]
+            '1': ['5', '7', '9'],
+            '2': ['1', '2', '7'],
+            '3': ['2', '3', '8'],
+            '4': ['8', '7', '2'],
+            '5': ['3', '4', '8'],
+            '6': ['4', '9', '8'],
+            '7': ['4', '6', '5'],
+            '8': ['5', '10', '11', '1'],
+            '9': ['9', '4', '5'],
+            '10': ['5', '1', '7']
             }
 
         self.boundaries = dict()
 
         self.boundaries[None] = {  # "open" boundaries
-                1: [10, 11, 1, 2],
-                2: [2, 3, 4]
+                0: ['10', '11', '1', '2'],
+                1: ['2', '3', '4']
         }
 
         self.boundaries[0] = {  # "land" boundaries
-            1: [4, 6],
-            2: [6,  5, 10]
+            0: ['4', '6'],
+            1: ['6',  '5', '10']
         }
 
-        self.boundaries[1] = {1: [7, 8, 9]}  # "interior" boundary
+        self.boundaries[1] = {0: ['7', '8', '9']}  # "interior" boundary
 
         self.grd = {
             'nodes': self.nodes,
@@ -57,10 +57,11 @@ class Gr3TestCase(unittest.TestCase):
 
     def test_writer(self):
         tmpdir = tempfile.TemporaryDirectory()
-        writer(self.grd, pathlib.Path(tmpdir.name) / 'hgrid.gr3')
+        self.assertIsNone(
+            writer(self.grd, pathlib.Path(tmpdir.name) / 'hgrid.gr3'))
 
     def test_reader(self):
-
+        # write a sample test file
         g = self.grd['description'] + "\n"
         g += f"{len(self.grd['elements'])} {len(self.grd['nodes'])}\n"
         for id, ((x, y), values) in self.nodes.items():
@@ -105,9 +106,9 @@ class Gr3TestCase(unittest.TestCase):
         tmpfile = tempfile.NamedTemporaryFile()
         with open(tmpfile.name, 'w') as f:
             f.write(g)
-        reader(pathlib.Path(tmpfile.name))
+        self.assertDictEqual(reader(pathlib.Path(tmpfile.name)), self.grd)
 
-    def test_gr3_overwrite(self):
+    def test_overwrite(self):
         tmpdir = tempfile.TemporaryDirectory()
         writer(self.grd, pathlib.Path(tmpdir.name) / 'hgrid.gr3')
         self.assertRaises(
