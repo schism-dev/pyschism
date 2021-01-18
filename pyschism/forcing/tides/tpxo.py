Sign fix for TPXO interpolation as noted by C. Seaton.

pyschism/forcing/tides/tpxo.py
@@ -54,7 +54,8 @@ class TPXO:
     def get_elevation(self, constituent, vertices):
         index = self.constituents.index(constituent.lower().capitalize())
         array = read_elevation_file(self._hfile, index)
-        xq = np.asarray([x + 360. for x in vertices[:, 0] if x < 0]).flatten()
+        xq = np.asarray(
+            [x + 360. if x < 0. else x for x in vertices[:, 0]]).flatten()
         yq = vertices[:, 1].flatten()
         xi, yi = np.meshgrid(self.x, self.y)
         dx = np.mean(np.diff(self.x))