48abdce9daba086e0a0b6a694e6a486ba8883569
Jaime R Calzada <jreniel@gmail.com>
2021-08-27 09:59:38 -0400

    updated test_Florence.py

--- tests/local/NWM_hindcast_test/test_Florence.py
+++ tests/local/NWM_hindcast_test/test_Florence.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 
 # logging.getLogger('pyschism').setLevel(logging.DEBUG)
 
-output_directory = pathlib.Path('Florence')
+output_directory = pathlib.Path("Florence")
 
 startdate = datetime(2018, 8, 24)
 
@@ -24,11 +24,11 @@ rnday = 36
 hgrid = Hgrid.open(
     "hgrid.gr3",
     # os.getenv('NWM_TEST_MESH'),
-    crs="epsg:4326"
+    crs="epsg:4326",
 )
 
-sources_pairings = output_directory / 'sources.json'
-sinks_pairings = output_directory / 'sinks.json'
+sources_pairings = output_directory / "sources.json"
+sinks_pairings = output_directory / "sinks.json"
 
 if all([sources_pairings.is_file(), sinks_pairings.is_file()]) is False:
     pairings = NWMElementPairings(hgrid)
@@ -38,41 +38,50 @@ if all([sources_pairings.is_file(), sinks_pairings.is_file()]) is False:
 else:
     pairings = NWMElementPairings.load_json(hgrid, sources_pairings, sinks_pairings)
 
-nwm = NationalWaterModel(
-    aggregation_radius=4000,
-    pairings=pairings
+nwm = NationalWaterModel(aggregation_radius=4000, pairings=pairings)
+
+nwm.write(
+    output_directory / f"rnday{rnday}",
+    hgrid,
+    startdate,
+    rnday,
+    overwrite=True,
 )
+
+exit()
+
+# test how much longer it takes depending on the value of rnday
 
 
 def Fibonacci(n):
-   
+
     # Check if input is 0 then it will
     # print incorrect input
     if n < 0:
         print("Incorrect input")
- 
+
     # Check if n is 0
     # then it will return 0
     elif n == 0:
         return 0
- 
+
     # Check if n is 1,2
     # it will return 1
     elif n == 1 or n == 2:
         return 1
- 
+
     else:
-        return Fibonacci(n-1) + Fibonacci(n-2)
+        return Fibonacci(n - 1) + Fibonacci(n - 2)
 
 
 for i in range(2, 10):
     start = datetime.now()
     rnday = Fibonacci(i)
     nwm.write(
-        output_directory / f'rnday{rnday}',
+        output_directory / f"rnday{rnday}",
         hgrid,
         startdate,
         Fibonacci(i),
         overwrite=True,
     )
-    print(f'Took {datetime.now() - start} for {rnday}')
+    print(f"Took {datetime.now() - start} for {rnday}")
