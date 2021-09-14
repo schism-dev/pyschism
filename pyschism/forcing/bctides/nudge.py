de7f13ea21fb82d9e6f9307dcf94fae00baa8baf
Jaime R Calzada <jreniel@gmail.com>
2021-09-14 13:45:58 -0400

    indexing fix on nudge.py

--- pyschism/forcing/bctides/nudge.py
+++ pyschism/forcing/bctides/nudge.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 import logging
+from time import time
+
 
 from numba import prange, jit
 import numpy as np
@@ -11,47 +13,44 @@ logger = logging.getLogger(__name__)
 
 
 class Nudge(Gr3Field):
-
     def __init__(self, bctides, data_source, rlmax=1.5, rnu_day=0.25):
 
         @jit(nopython=True, parallel=True)
         def compute_nudge(lon, lat, opbd, out):
 
-            nnode = lon.shape[0]
+            rnu_max = 1.0 / rnu_day / 86400.0
 
-            rnu_max = 1./rnu_day/86400.
-
-            for idn in prange(nnode):
+            for idn in prange(lon.shape[0]):
                 if idn in opbd:
                     rnu = rnu_max
-                    distmin = 0.
+                    distmin = 0.0
                 else:
                     distmin = np.finfo(np.float64).max
                     for j in opbd:
                         rl2 = np.sqrt(
-                            np.square(lon[idn] - lon[j])
-                            + np.square(lat[idn] - lat[j])
-                            )
+                            np.square(lon[idn] - lon[j]) + np.square(lat[idn] - lat[j])
+                        )
                         if rl2 < distmin:
                             distmin = rl2
-                rnu = 0.
+                rnu = 0.0
                 if distmin <= rlmax:
-                    rnu = (1-distmin/rlmax)*rnu_max
+                    rnu = (1 - distmin / rlmax) * rnu_max
                 out[idn] = rnu
+
         out = np.zeros(bctides.hgrid.values.shape)
-        xy = bctides.hgrid.get_xy(crs='epsg:4326')
+        xy = bctides.hgrid.get_xy(crs="epsg:4326")
         opbd = []
         for boundary in bctides.gdf.itertuples():
             forcing = getattr(boundary, self.bctype)
             if forcing.nudge is True:
                 opbd.extend(list(boundary.indexes))
         opbd = np.array(opbd)
+
         if len(opbd) > 0:
-            logger.info(f'Begin compute_nudge for {self.name}.')
-            from time import time
+            logger.info(f"Begin compute_nudge for {self.name}.")
             start = time()
             compute_nudge(xy[:, 0], xy[:, 1], opbd, out)
-            logger.info(f'compute_nudge took {time()-start} seconds.')
+            logger.info(f"compute_nudge took {time()-start} seconds.")
         super().__init__(
             nodes={i: (coord, out[i]) for i, coord in enumerate(bctides.hgrid.coords)},
             elements=bctides.hgrid.elements.elements,
@@ -59,37 +58,39 @@ class Nudge(Gr3Field):
             crs=bctides.hgrid.crs,
         )
 
+        self.data_source = data_source
+
+    def write()
+
     @property
     @abstractmethod
     def name(self):
-        ''''''
+        """ """
 
     @property
     @abstractmethod
     def bctype(self):
-        ''''''
+        """ """
 
 
 class TEM_Nudge(Nudge):
-
     @property
     def name(self):
-        return 'temperature'
+        return "temperature"
 
     @property
     def bctype(self):
-        return 'itetype'
+        return "itetype"
 
 
 class SAL_Nudge(Nudge):
-
     @property
     def name(self):
-        return 'salinity'
+        return "salinity"
 
     @property
     def bctype(self):
-        return 'isatype'
+        return "isatype"
 
 
 TempNudge = TEM_Nudge
