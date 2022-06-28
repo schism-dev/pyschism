28622b0d75d97dc9c256dfde4c63e2a14d819675
cuill <cui86ll@gmail.com>
2022-06-28 09:57:42 -0400

    Add dunamai back

--- setup.py
+++ setup.py
@@ -9,20 +9,23 @@ subprocess.check_call(
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'wheel'])
 
-#try:
-#    from dunamai import Version
-#except ImportError:
-#    subprocess.check_call(
-#            [sys.executable, '-m', 'pip', 'install', 'dunamai']
-#    )
-#    from dunamai import Version  # type: ignore[import]
+try:
+    from dunamai import Version
+except ImportError:
+    subprocess.check_call(
+            [sys.executable, '-m', 'pip', 'install', 'dunamai']
+    )
+    from dunamai import Version  # type: ignore[import]
 
-#try:
-#    print(Version.from_any_vcs().serialize())
-#    version = Version.from_any_vcs().serialize()
-#except RuntimeError:
-#    version = '0.0.0'
-
+try:
+    version = Version.from_any_vcs().serialize()
+except RuntimeError:
+    version = '0.0.0'
+except ValueError as e:
+    if "time data '%cI' does not match format '%Y-%m-%dT%H:%M:%S%z'" in str(e):
+    version = '0.0.0'
+    else:
+        raise
 
 class BuildSchism(setuptools.Command):
 
@@ -82,7 +85,7 @@ conf = setuptools.config.read_configuration(parent / 'setup.cfg')
 meta = conf['metadata']
 setuptools.setup(
     name=meta['name'],
-    version=meta['version'],
+    version=version,
     author=meta['author'],
     author_email=meta['author_email'],
     description=meta['description'],
