8107b86cad31ae0f3708b2892f3f518e8608c82a
cuill <cui86ll@gmail.com>
2023-11-10 10:14:42 -0500

    update example scripts for gfs and hrrr

--- examples/Sflux/gen_sflux_hrrr3.py
+++ examples/Sflux/gen_sflux_hrrr3.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timedelta
 from time import time
 import logging
+import pathlib
+
+import numpy as np
+import pandas as pd
+from matplotlib.transforms import Bbox
 
 from pyschism.mesh.hgrid import Hgrid
 from pyschism.forcing.nws.nws2.hrrr3 import HRRR
@@ -17,11 +22,24 @@ logging.getLogger('pyschism').setLevel(log_level)
 
 if __name__ == "__main__":
     t0 = time()
-    rnday = 1
-    record = 1
-    startdate = nearest_cycle()
-    hgrid = Hgrid.open('../../static/hgrid.gr3', crs='epsg:4326')
+
+    #now = datetime.now()
+    #last_cycle = np.datetime64(pd.DatetimeIndex([now-timedelta(hours=2)]).floor('6H').values[0], 'h').tolist()
+    #print(last_cycle)
+
+    #start = (last_cycle - timedelta(days=1)).replace(hour=0)
+    #end = last_cycle + timedelta(days=2)
+
+    #rndelta = end -start
+    #rnday = rndelta.total_seconds() / 86400.
+    start = datetime(2023, 10, 26)
+    rnday = 2
+    hgrid = Hgrid.open('./hgrid.gr3', crs='epsg:4326')
+    #outdir = path = pathlib.Path('./HRRR_2017_2')
+    #bbox = Bbox.from_extents(-162, 60.79, -143, 69.1)
+    
 
     pscr='/sciclone/pscr/lcui01/HRRR/'
-    hrrr = HRRR(start_date=startdate, rnday=rnday, pscr=pscr, record=record, bbox=hgrid.bbox)
-    print(f'It took {(time()-t0)/60} mins to process {rnday} days, {record*24} files/day')
+    hrrr = HRRR(level=2, region='alaska', pscr=pscr, bbox=bbox)
+    hrrr.write(start_date=start, rnday=rnday, air=True, prc=True, rad=True)
+    print(f'It took {(time()-t0)/60} mins to process {rnday} days!')
