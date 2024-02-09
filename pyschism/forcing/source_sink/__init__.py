7380c09ef5bb1e157c606258ddfd378141d9f184
Jason Ducker <81377226+jduckerOWP@users.noreply.github.com>
2024-02-09 12:06:24 -0500

    Update __init__.py
    Include NextGen Source Sink Creation Option.

--- pyschism/forcing/source_sink/__init__.py
+++ pyschism/forcing/source_sink/__init__.py
@@ -8,12 +8,16 @@ from pyschism.forcing.source_sink.base import (
     SourceSink,
 )
 from pyschism.forcing.source_sink.nwm import NationalWaterModel
+from pyschism.forcing.source_sink.ngen import NextGen
+
 
 NWM = NationalWaterModel
+NGen = NextGen
 
 __all__ = [
     # "Hydrology",
     "NationalWaterModel",
+    "NextGen",
     # "Sources",
     # "Sinks",
     "Msource",
@@ -21,4 +25,5 @@ __all__ = [
     "Vsink",
     "SourceSink",
     'NWM',
+    'NGen',    
 ]
